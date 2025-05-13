b997a38fba0e6aaba03f5ace3054b05b4f71a9ab
lgpereir <lgp@if.ufrgs.br>
2025-05-13 00:09:21 +0000

    testing code

---
+++ ger.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+./sim -m ../run10.mac -t 24 -o air10g010  -s  10
+./sim -m ../run10.mac -t 24 -o air10g100  -s  100
+./sim -m ../run10.mac -t 24 -o air10g200  -s  200
+./sim -m ../run10.mac -t 24 -o air10g300  -s  300
+./sim -m ../run10.mac -t 24 -o air10g400  -s  400
+./sim -m ../run10.mac -t 24 -o air10g500  -s  500
+./sim -m ../run10.mac -t 24 -o air10g600  -s  600
+./sim -m ../run10.mac -t 24 -o air10g700  -s  700
+./sim -m ../run10.mac -t 24 -o air10g800  -s  800
+./sim -m ../run10.mac -t 24 -o air10g900  -s  900
+
+
+
